d67227913ebd83798485be7410a272941c9908e3
Ryan <straightwind0726@gmail.com>
2023-05-19 07:40:15 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const bodyWidth = document.body.offsetWidth;
+
+function getMargin(a) {
+  return Math.floor(Math.random() * a);
+}
+
+function makeSnow() {
+  const snow = document.createElement('div');
+  snow.setAttribute('id','snow');
+  snow.style.marginLeft = `${getMargin(bodyWidth)}px`;
+  document.body.appendChild(snow);
+}
+
+for(let i = 0; i < 200; i++) {
+  makeSnow();
+}
