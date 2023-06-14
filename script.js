d599de83e63da785ce03cbb9d6fb579682c8849c
Ryan <straightwind0726@gmail.com>
2023-06-14 21:42:38 +0900

    [Update] add css code -- global style

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function getMargin(a) {
 }
 
 function makeSnow() {
-  const snow = document.createElement('div');
-  snow.setAttribute('id','snow');
+  const snow = document.createElement("div");
+  snow.setAttribute("id", "snow");
   snow.style.marginLeft = `${getMargin(bodyWidth)}px`;
   document.body.appendChild(snow);
 }
 
-for(let i = 0; i < 200; i++) {
+for (let i = 0; i < 200; i++) {
   makeSnow();
 }
